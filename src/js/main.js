e75a1774920b6a350c044adbe83eed9ec22a3ce4
Arthur Bobadilla Franchi <arthurbobadilla2006@gmail.com>
2024-08-21 15:25:04 -0300

    Organzando

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,8 @@ const swiper = new Swiper('.swiper', {
     speed: 400,
     spaceBetween: 100,
     slidesPerView: 4,
+    pagination:{
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
 });
